6464eef6a60682390bb8ac1d96f0992bd02e90b3
BaptFR <baptistesalazar@gmail.com>
2025-04-23 11:13:08 +0200

    rules diff

---
+++ index.py
@@ -0,0 +1,16 @@
+# Règles Black Jack
+# Distribution cartes règles :
+# US
+# EU
+#
+# Main Joueur = HP
+# Main Dealer = HD
+#
+# Situations règles US ?:
+#   Si En dessous Dealer hand = Perdu
+#   Si HP > HD  = gagné
+#   Si HP < HD  = gagné
+#   Si handJ au dessus de 21 = perdu 
+#
+#
+#
