b3b7c39640e539cab0fa3c3c0a35ba6c795e4223
BaptFr <baptistesalazar@gmail.com>
2025-04-23 22:13:59 +0200

    feat: créations des classe :  paquet shuffleet méthode tirer, cartes: couleur, valeur

---
+++ jeu/blackjack.py
@@ -0,0 +1,31 @@
+import random 
+
+class Carte:
+    def __init__(self, valeur, couleur):
+        self.valeur = valeur
+        self.couleur = couleur
+
+    def __repr__(self):
+        return f"{self.valeur} de {self.couleur}"
+
+class Paquet:
+    def __init__(self):
+        couleurs = ["Coeur", "Carreau", "Trèfle", "Pique"]
+        valeurs = [ "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "R", "As"]
+        #Liste cartes
+        self.cartes = []
+
+        #Création paquet de cartes x52
+        for couleur in couleurs:
+            for valeur in valeurs:
+                self.cartes.append(Carte(valeur, couleur))
+        #mélange alétaoire liste
+        random.shuffle(self.cartes)
+
+    def tirer(self):
+        return self.cartes.pop()
+
+class Jeu:
+    def __init__(self):
+        self.joueur = []
+        self.croupier = []
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from
+import tkinter as tk
+from jeu.blackjack import Jeu, Paquet
 
-# Règles Black Jack
-# Distribution cartes règles :
-# US
-# EU
-#
-# Main Joueur = HP
-# Main Dealer = HD
-#
-# Situations règles US ?:
-#   Si En dessous Dealer hand = Perdu
-#   Si HP > HD  = gagné
-#   Si HP < HD  = gagné
-#   Si handJ au dessus de 21 = perdu 
-#
-#
-#
+root = tk.Tk()
+root.title("BlackJack")
+root.geometry("400x300")
+
+jeu = Jeu()
+paquet = Paquet()
+carte = paquet.tirer() #class pour pop = tirer
+
+#test
+label = tk.Label(root, text="BlackJack", font=("Arial", 24))
+label.pack()
+
+carte_label = tk.Label(root, text=str(carte), font=("Arial", 18))
+carte_label.pack()
+
+print(jeu.joueur)
+
+root.mainloop()
+
+
